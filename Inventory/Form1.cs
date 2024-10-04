73968411b31fc0d853eac3077a1f414a2a31599d
Ar-KIT <rotoni.287064@pasay-edsa.sti.edu.ph>
2024-10-04 17:00:58 +0800

    Add project files.

---
+++ Inventory/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Inventory
+{
+    public partial class frmAddProduct : Form
+    {
+        BindingSource ShowProductList;
+        private string _ProductName, _Category, _MfgDate, _ExpDate, _Description;
+        private int _Quantity;
+        private double _SellPrice;
+        public frmAddProduct()
+        {
+            InitializeComponent();
+            ShowProductList = new BindingSource();
+        }
+        private void frmAddProduct_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProductCategory = new string[]
+            {
+                "Beverages",
+                "Bread/Bakery",
+                "Canned Jarred Goods",
+                "Dairy",
+                "Frozen Goods",
+                "Meat",
+                "Personal Care",
+                "Other"
+            };
+            foreach(string category in ListOfProductCategory)
+            {
+                cbCategory.Items.Add(category);
+            }
+        }
+        public string Product_Name(string name)
+        {
+            if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+                throw new StringFormatException("Invalid input at 'Product Name'");
+            return name;
+        }
+        public int Quantity(string qty)
+        {
+            if (!Regex.IsMatch(qty, @"^[0-9]"))
+                throw new NumberFormatException("Invalid Quantity. Must Input 'INTEGER'");
+            return Convert.ToInt32(qty);
+        }
+        public double SellingPrice(string price)
+        {
+            if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+                throw new CurrencyFormatException("Invalid Selling Price; Must Input 'DOUBLE'");
+            return Convert.ToDouble(price);
+        }
+        public class NumberFormatException : Exception
+        {
+            public NumberFormatException(string message) : base(message) { }
+        }
+        public class StringFormatException : Exception
+        {
+            public StringFormatException(string message) : base(message) { }
+        }
+        public class CurrencyFormatException : Exception
+        {
+            public CurrencyFormatException(string message) : base(message) { }
+        }
+
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _ProductName = Product_Name(txtProductName.Text);
+                _Category = cbCategory.Text;
+                _MfgDate = dtPickerMfgDate.Value.ToString("yyyy-MM-dd");
+                _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
+                _Description = richTxtDescription.Text;
+                _Quantity = Quantity(txtQuantity.Text);
+                _SellPrice = SellingPrice(txtSellPrice.Text);
+                ShowProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate, _ExpDate, _SellPrice, _Quantity, _Description));
+                gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                gridViewProductList.DataSource = ShowProductList;
+            }
+            catch (NumberFormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (StringFormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (CurrencyFormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Extra Code: Clear all Inputs
+                txtProductName.Clear();
+                txtQuantity.Clear();
+                txtSellPrice.Clear();
+                richTxtDescription.Clear();
+                cbCategory.SelectedIndex = -1;
+                dtPickerMfgDate.Value = DateTime.Now;
+                dtPickerExpDate.Value = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ Inventory/ProductClass.cs
@@ -0,0 +1,34 @@
+﻿namespace Inventory
+{
+    public class ProductClass
+    {
+        private int _Quantity;
+        private double _SellingPrice;
+        private string _ProductName, _Category, _ManufacturingDate, _ExpirationDate, _Description;
+
+        public ProductClass(string ProductName, string Category, string MfgDate, string ExpDate, double Price, int Quantity, string Description)
+        {
+            this._Quantity = Quantity;
+            this._SellingPrice = Price;
+            this._ProductName = ProductName;
+            this._Category = Category;
+            this._ManufacturingDate = MfgDate;
+            this._ExpirationDate = ExpDate;
+            this._Description = Description;
+        }
+        public string productName
+            { get { return _ProductName; } set { _ProductName = value; } }
+        public string category
+            { get { return _Category; } set { _Category = value; } }
+        public string manufacturingDate
+            { get { return _ManufacturingDate; } set { _ManufacturingDate = value; } }
+        public string expirationDate
+            { get { return _ExpirationDate; } set { _ExpirationDate = value; } }
+        public string description
+            { get { return _Description; } set {  this._Description = value; } }
+        public int quantity
+            { get { return _Quantity; } set { this._Quantity = value; } }
+        public double sellingPrice
+            { get { return _SellingPrice; } set { _SellingPrice = value; } }
+    }
+}
